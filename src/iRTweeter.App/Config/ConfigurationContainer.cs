a136375da6565727a8dfcd3f1c7e543e6cc0939b
Steve Hobbs <elkdanger@googlemail.com>
2015-01-19 22:15:33 +0000

    Bootstrapped web server and static files

---
+++ src/iRTweeter.App/Config/ConfigurationContainer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace iRTweeter.App.Config
+{
+    public class ConfigurationContainer : IConfig
+    {
+        /// <summary>
+        /// Loads the specified filename.
+        /// </summary>
+        /// <param name="filename">The filename.</param>
+        public static IConfig Load(string filename = "config.json")
+        {
+            var json = File.ReadAllText(filename);
+            var config = JsonConvert.DeserializeObject<ConfigurationContainer>(json);
+
+            return config;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the settings page should be opened once the server starts
+        /// </summary>
+        public bool RunSettingsOnStart { get; set; }
+
+        /// <summary>
+        /// Gets or sets the server settings
+        /// </summary>
+        [JsonConverter(typeof(ConcreteTypeConverter<ServerConfig>))]
+        public IServerConfig Server { get; set; }
+    }
+
+    /// <summary>
+    /// Configuration for the server
+    /// </summary>
+    public class ServerConfig : IServerConfig
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServerConfig"/> class.
+        /// </summary>
+        public ServerConfig()
+        {
+            this.Port = 8080;
+        }
+
+        /// <summary>
+        /// Gets or sets the port.
+        /// </summary>
+        public int Port { get; set; }
+
+        /// <summary>
+        /// Gets the host URI.
+        /// </summary>
+        public Uri GetHostUri()
+        {
+            return new UriBuilder("http", "localhost", this.Port).Uri;
+        }
+    }
+
+    /// Interfaces
+
+    public interface IConfig
+    {
+        bool RunSettingsOnStart { get; }
+        IServerConfig Server { get; }
+    }
+
+    public interface IServerConfig
+    {
+        int Port { get; }
+        Uri GetHostUri();
+    }
+
+    /// Converters
+
+    // http://blog.greatrexpectations.com/2012/08/30/deserializing-interface-properties-using-json-net/
+    internal class ConcreteTypeConverter<T> : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return true;
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return serializer.Deserialize<T>(reader);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            serializer.Serialize(writer, value);
+        }
+    }
+}
--- src/iRTweeter.App/Program.cs
+++ src/iRTweeter.App/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
+using iRTweeter.App.Config;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.Hosting;
+using Microsoft.Owin.StaticFiles;
+using Owin;
 
 namespace iRTweeter.App
 {
     static class Program
     {
         static IContainer components = new System.ComponentModel.Container();
+        private static IConfig config;
 
         /// <summary>
         /// The main entry point for the application.
@@ -17,21 +21,68 @@ namespace iRTweeter.App
         [STAThread]
         static void Main()
         {
+            BootstrapComponents();
+
+            config = ConfigurationContainer.Load();
+
+            var hostUri = config.Server.GetHostUri();
+
+            WebApp.Start<Startup>(hostUri.ToString());
+
+            if (config.RunSettingsOnStart)
+            {
+                Process.Start(hostUri.ToString());
+            }
+
+            Application.Run();
+        }
+
+        /// <summary>
+        /// Bootstraps the windows components.
+        /// </summary>
+        private static void BootstrapComponents()
+        {
             var trayIcon = new NotifyIcon(components);
 
             trayIcon.Icon = ((System.Drawing.Icon)(Properties.Resources.ResourceManager.GetObject("trayIcon")));
             trayIcon.Text = "iRTweeter";
             trayIcon.Visible = true;
 
-            var contextMenu = new ContextMenu( new [] {
+            var contextMenu = new ContextMenu(new[] {
                 new MenuItem("Settings"),
                 new MenuItem("-"),
-                new MenuItem("Exit", (sender, args) => Application.Exit())
+                new MenuItem("Exit", (sender, args) => {
+                    trayIcon.Visible = false;
+                    Application.Exit();
+                })
             });
 
             trayIcon.ContextMenu = contextMenu;
+        }
+    }
 
-            Application.Run();
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var fileSystem = new PhysicalFileSystem("www");
+
+            var fileServerOptions = new FileServerOptions
+            {
+                EnableDefaultFiles = true,
+                FileSystem = fileSystem,
+                StaticFileOptions =
+                {
+                    FileSystem = fileSystem,
+                    ServeUnknownFileTypes = false
+                },
+                DefaultFilesOptions =
+                {
+                    DefaultFileNames = new[] { "index.html" }
+                }
+            };
+
+            app.UseFileServer(fileServerOptions);
         }
     }
 }
