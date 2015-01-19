0e2393634d63b7499e5b1e8131c8b083a80c5742
Steve Hobbs <elkdanger@googlemail.com>
2015-01-19 21:24:09 +0000

    Basic bootstrapping

---
+++ src/iRTweeter.App/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iRTweeter.App
+{
+    static class Program
+    {
+        static IContainer components = new System.ComponentModel.Container();
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var trayIcon = new NotifyIcon(components);
+
+            trayIcon.Icon = ((System.Drawing.Icon)(Properties.Resources.ResourceManager.GetObject("trayIcon")));
+            trayIcon.Text = "iRTweeter";
+            trayIcon.Visible = true;
+
+            var contextMenu = new ContextMenu( new [] {
+                new MenuItem("Settings"),
+                new MenuItem("-"),
+                new MenuItem("Exit", (sender, args) => Application.Exit())
+            });
+
+            trayIcon.ContextMenu = contextMenu;
+
+            Application.Run();
+        }
+    }
+}
