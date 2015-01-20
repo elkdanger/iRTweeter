4aba488ca61872a8f534bca8b376d13a2a44cd70
Steve Hobbs <elkdanger@googlemail.com>
2015-01-20 00:25:52 +0000

    Basic settings page

--- src/iRTweeter.App/www/js/app.js
+++ src/iRTweeter.App/www/js/app.js
@@ -26,6 +26,16 @@
         }])
         .controller('SettingsController', ['$scope', function ($scope) {
 
+            $scope.settings = {
+                port: 6060,
+                runOnWindowsStart: true,
+                autoOpenDash: false
+            };
+
+            $scope.save = function (settings) {
+                console.log(settings);
+            };
+
         }])
         .controller('AboutController', ['$scope', function ($scope) {
 
