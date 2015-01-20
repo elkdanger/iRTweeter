407e8c08b6d9d6a5783bd6cc6201dea4475baa58
Steve Hobbs <elkdanger@googlemail.com>
2015-01-20 00:05:56 +0000

    Added settings and about pages

--- src/iRTweeter.App/www/js/app.js
+++ src/iRTweeter.App/www/js/app.js
@@ -2,15 +2,33 @@
 
     var app = angular.module('irtweeter', ['ngRoute'])
         .config(function ($routeProvider) {
-            
-            $routeProvider.when('/', {
-                templateUrl: '/views/index.html',
-                controller: 'HomeController'
-            });
+
+            $routeProvider
+                .when('/', {
+                    templateUrl: '/views/index.html',
+                    controller: 'HomeController'
+                })
+                .when('/settings', {
+                    templateUrl: 'views/settings.html',
+                    controller: 'SettingsController'
+                })
+                .when('/about', {
+                    templateUrl: '/views/about.html',
+                    controller: 'AboutController'
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
 
         })
-        .controller("HomeController", ['$scope', '$routeProvider', function ($scope, $routeProvider) {
-            console.log($routeProvider);
+        .controller('HomeController', ['$scope', function ($scope) {
+
+        }])
+        .controller('SettingsController', ['$scope', function ($scope) {
+
+        }])
+        .controller('AboutController', ['$scope', function ($scope) {
+
         }]);
 
 })();
