1ebf1c2cac0c9c071152eda442bb0f9f4cca3a3a
Steve Hobbs <elkdanger@googlemail.com>
2015-01-28 15:42:51 +0000

    Added injection stage

---
+++ src/iRTweeter.App/Tasks/inject.js
@@ -0,0 +1,15 @@
+﻿var gulp = require('gulp');
+var inject = require('gulp-inject');
+
+gulp.task('inject', function () {
+
+    var target = gulp.src('./www/index.html');
+
+    var sources = gulp.src([
+        './www/js/controllers/**/*.js',
+        './www/js/services/**/*.js'
+    ], { read: false });
+
+    return target.pipe(inject(sources))
+        .pipe(gulp.dest('./www'));
+});
--- src/iRTweeter.App/gulpfile.js
+++ src/iRTweeter.App/gulpfile.js
@@ -1,10 +1,12 @@
 /// <vs AfterBuild='build' SolutionOpened='watch_sass' />
 var gulp = require('gulp');
+var inject = require('gulp-inject');
 
 var sass = require('./Tasks/sass');
 var watch = require('./Tasks/watch')
 var bower = require('./Tasks/bower');
 var scripts = require('./Tasks/scripts');
+var inject = require('./Tasks/inject');
 
 // Primary build task
-gulp.task('build', ['scripts', 'sass']);
+gulp.task('build', ['scripts', 'sass', 'inject']);
